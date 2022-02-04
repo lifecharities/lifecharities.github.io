9b118db9d85f57b6deaab5ef6523639bf211127f
William Faircloth <willuhm@protonmail.com>
2022-02-04 10:46:33 -0500

    Upload core

---
+++ CONTRIBUTING.md
@@ -0,0 +1,24 @@
+# Contributing to Life Charities
+There are many charities and organizations that support the fight for life. Your contribution can help grow this list.
+
+## Formatting
+Current JSON list format:
+```json
+{
+  "Organization": "Name of the organization."
+  "Homepage Link": "Link to the organization's home page."
+  "Donation Link": "Link to the organization's donation page."
+}
+```
+
+All charities are listed in JSON format under [organizations.js](https://github.com/lifecharities/lifecharities.github.io/blob/main/organizations.js)
+
+## Guidelines
+* The maintainer(s) may approve or deny a request at their own discretion.
+* All charities must be a pro-life or maternal organization.
+* All links must be in their simpliest form; keep the path as short as possible.
+* All charities must be in lowercase.
+* Please only submit one charity at a time.
+
+## Other Contributions
+Contributions outside the list of charities, such as code contributions, are also appreciated.
---
+++ organizations.js
@@ -0,0 +1,148 @@
+const orgs = [
+  {
+    "Organization": "live action",
+    "Homepage Link": "https://liveaction.org",
+    "Donation Link": "https://give.liveaction.org/",
+  },
+  {
+    "Organization": "life legal defense foundation",
+    "Homepage Link": "https://lifelegaldefensefoundation.org/",
+    "Donation Link": "https://lifelegaldefensefoundation.org/donate/",
+  },
+  {
+    "Organization": "american life league",
+    "Homepage Link": "https://www.all.org/",
+    "Donation Link": "https://americanlifeleague.revv.co/donate?utm_source=website&utm_campaign=all",
+  },
+  {
+    "Organization": "national right to life",
+    "Homepage Link": "https://www.nrlc.org/",
+    "Donation Link": "https://www.nrlc.org/donate",
+  },
+  {
+    "Organization": "march for life",
+    "Homepage Link": "https://marchforlife.org/",
+    "Donation Link": "https://marchforlife.org/give-today/",
+  },
+  {
+    "Organization": "students for life",
+    "Homepage Link": "https://studentsforlife.org",
+    "Donation Link": "https://studentsforlifeofamerica.givingfuel.com/supporters",
+  },
+  {
+    "Organization": "susan b. anthony list",
+    "Homepage Link": "https://www.sba-list.org/",
+    "Donation Link": "https://secure.anedot.com/sba/donate",
+  },
+  {
+    "Organization": "let them live",
+    "Homepage Link": "https://www.letthemlive.org/",
+    "Donation Link": "https://donate.letthemlive.org/",
+  },
+  {
+    "Organization": "concerned women for america",
+    "Homepage Link": "https://concernedwomen.org",
+    "Donation Link": "https://concernedwomen.org/donate/",
+  },
+  {
+    "Organization": "human life international",
+    "Homepage Link": "https://www.hli.org/about-us/",
+    "Donation Link": "https://www.hli.org/donations/",
+  },
+  {
+    "Organization": "save the storks",
+    "Homepage Link": "https://www.savethestorks.com",
+    "Donation Link": "https://donate.savethestorks.com/donate",
+  },
+  {
+    "Organization": "prolife across america",
+    "Homepage Link": "https://prolifeacrossamerica.org/",
+    "Donation Link": "https://prolifeacrossamerica.org/donate/",
+  },
+  {
+    "Organization": "rachel's vineyard",
+    "Homepage Link": "https://www.rachelsvineyard.org/",
+    "Donation Link": "https://www.rachelsvineyard.org/donate/index.aspx",
+  },
+  {
+    "Organization": "mercy multiplied",
+    "Homepage Link": "https://mercymultiplied.com/",
+    "Donation Link": "https://mercymultiplied.com/donate",
+  },
+  {
+    "Organization": "sisters for life",
+    "Homepage Link": "https://sistersoflife.org/",
+    "Donation Link": "https://sistersoflife.org/donate/",
+  },
+  {
+    "Organization": "anglicans for life",
+    "Homepage Link": "https://anglicansforlife.org/",
+    "Donation Link": "https://www.myegiving.com/App/Form/3c2cedbe-2d1e-4db4-b8ee-adb08d727d8a",
+  },
+  {
+    "Organization": "american family association",
+    "Homepage Link": "https://afa.net/",
+    "Donation Link": "https://afa.net/support-us/make-a-donation/",
+  },
+  {
+    "Organization": "heartbeat international",
+    "Homepage Link": "https://heartbeatinternational.org",
+    "Donation Link": "https://heartbeatinternational.org/donate",
+  },
+  {
+    "Organization": "care net",
+    "Homepage Link": "https://www.care-net.org/",
+    "Donation Link": "https://www.care-net.org/donate",
+  },
+  {
+    "Organization": "operation save america",
+    "Homepage Link": "https://www.operationsaveamerica.org/",
+    "Donation Link": "https://store.operationsaveamerica.org/donate.asp",
+  },
+  {
+    "Organization": "american center for law & justice",
+    "Homepage Link": "https://aclj.org/",
+    "Donation Link": "https://aclj.org/donate",
+  },
+  {
+    "Organization": "the american tfp",
+    "Homepage Link": "https://www.tfp.org/",
+    "Donation Link": "https://www.tfp.org/act/donate/",
+  },
+  {
+    "Organization": "americans united for life",
+    "Homepage Link": "https://aul.org/",
+    "Donation Link": "https://aul.org/give/",
+  },
+  {
+    "Organization": "bound4life",
+    "Homepage Link": "https://www.bound4life.com/",
+    "Donation Link": "https://www.bound4life.com/give-now",
+  },
+  {
+    "Organization": "virginia society for human life",
+    "Homepage Link": "https://vshl.org/",
+    "Donation Link": "https://vshl.org/ways-to-donate",
+  },
+  {
+    "Organization": "the family foundation of virginia",
+    "Homepage Link": "https://www.familyfoundation.org/",
+    "Donation Link": "https://www.familyfoundation.org/contribute",
+  },
+  {
+    "Organization": "secular pro life",
+    "Homepage Link": "https://secularprolife.org/",
+    "Donation Link": "https://secularprolife.org/donate/",
+  },
+  {
+    "Organization": "population research institute",
+    "Homepage Link": "https://www.pop.org/",
+    "Donation Link": "https://www.pop.org/donate/standard/",
+  },
+  {
+    "Organization": "priests for life",
+    "Homepage Link": "https://www.priestsforlife.org/",
+    "Donation Link": "https://www.priestsforlife.org/donate/",
+  },
+  /* Insert Charity Above This Line */
+];
