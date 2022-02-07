b6724af4ec2c2472bb590c9a6a5d77ca1e9a0aed
willuhm-js <willuhm@protonmail.com>
2022-02-07 00:43:22 +0000

    Deploying to gh-pages from @ lifecharities/lifecharities.github.io@926ea149e74f5a4fa5c698697cef934e2f29fc62 🚀

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ $(document).ready(function () {
   orgs.forEach(orgGroup => {
     const item =
       `<div class="d-flex justify-content-center align-items-center m-5 fs-4">
-      <div class="btn-group">
-      <a target="_blank" href="${orgGroup["Homepage Link"]}" class="btn btn-lg btn-outline-danger border-3 fst-italic"><i class="fas fa-hand-holding-heart"></i> ${orgGroup.Organization}</a>
-      <a target="_blank" href="${orgGroup["Donation Link"]}" class="text-reset btn btn-lg btn-danger"><i class="fas fa-hand-holding-usd"></i></a>
-      </div>
-    </div>`;
+        <div class="btn-group">
+          <a target="_blank" href="${orgGroup["Homepage Link"]}" class="btn btn-lg btn-outline-danger border-3 fst-italic">
+            <i class="fas fa-hand-holding-heart"></i> ${orgGroup.Organization}</a>
+          <a target="_blank" href="${orgGroup["Donation Link"]}" class="text-reset btn btn-lg btn-danger d-flex align-items-center justify-content-center">
+            <i class="fas fa-hand-holding-usd"></i>
+          </a>
+        </div>
+      </div>`;
     $("#charities").append(item)
   });;
 });
