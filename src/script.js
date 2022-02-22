59efd37b379b6c605a4c19a28813b6e399afbdff
William Faircloth <willuhmjs@gmail.com>
2022-02-22 10:52:10 -0500

    Replace all instances of organization with charity

--- src/script.js
+++ src/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   function compareStrings(a, b) { return (a < b) ? -1 : (a > b) ? 1 : 0; }
 
-  orgs.sort(function (a, b) {
-    return compareStrings(a["Organization"], b["Organization"]);
+  charities.sort(function (a, b) {
+    return compareStrings(a["Charity"], b["Charity"]);
   })
-  orgs.forEach(orgGroup => {
+  charities.forEach(charGroup => {
     const item =
       `<div class="d-flex justify-content-center align-items-center m-5 fs-4">
         <div class="btn-group">
-          <a target="_blank" href="${orgGroup["Homepage Link"]}" class="btn btn-lg btn-outline-danger border-3 fst-italic">
-            <i class="fas fa-hand-holding-heart"></i> ${orgGroup.Organization}</a>
-          <a target="_blank" href="${orgGroup["Donation Link"]}" class="text-reset btn btn-lg btn-danger d-flex align-items-center justify-content-center">
+          <a target="_blank" href="${charGroup["Homepage Link"]}" class="btn btn-lg btn-outline-danger border-3 fst-italic">
+            <i class="fas fa-hand-holding-heart"></i> ${charGroup.Charity}</a>
+          <a target="_blank" href="${charGroup["Donation Link"]}" class="text-reset btn btn-lg btn-danger d-flex align-items-center justify-content-center">
             <i class="fas fa-hand-holding-usd"></i>
           </a>
         </div>
