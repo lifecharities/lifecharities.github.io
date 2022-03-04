3a71f3442a2385e21f344056d4e407c10f50f96c
William Faircloth <willuhmjs@gmail.com>
2022-03-04 11:09:42 -0500

    Add continent validation

--- tests/main.js
+++ tests/main.js
@@ -4,13 +4,21 @@ const c = require("centra");
 const path = require("path");
 const fs = require("fs");
 
+
 // read the file and convert it to a node module
 let tmpCode = "module.exports = " + fs.readFileSync(path.resolve(__dirname, "../charities.js"), "utf-8").slice(13);
 fs.writeFileSync(path.join(__dirname, "tmp.js"), tmpCode);
 
 const charities = require("./tmp.js");
-const lastChar = charities[charities.length - 1];
 
+console.log("Attempting to validate continents...");
+const continents = ["americas", "oceania", "europe", "africa", "asia"];
+charities.forEach(charity => {
+  if (!continents.includes(charity["Continent"])) throw Error ("Invalid continent");
+})
+
+console.log("Attempting to validate links...");
+const lastChar = charities[charities.length - 1];
 (async () => {
   await c(lastChar["Homepage Link"]).send();
   await c(lastChar["Donation Link"]).send();
